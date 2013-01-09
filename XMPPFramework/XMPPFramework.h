e303c2fbc87921b79455363d7908818702d6f6ed
Indragie Karunaratne <i@indragie.com>
2013-01-08 21:45:16 -0700

    Moved stuff around and made a static lib project

---
+++ XMPPFramework/XMPPFramework.h
@@ -0,0 +1,18 @@
+//
+//  XMPPFramework.h
+//  XMPPFramework
+//
+//  Created by Indragie Karunaratne on 2013-01-08.
+//  Copyright (c) 2013 nonatomic. All rights reserved.
+//
+
+#import "XMPP.h"
+#import "XMPPReconnect.h"
+#import "XMPPRoster.h"
+#import "XMPPRosterMemoryStorage.h"
+#import "XMPPvCardTempModule.h"
+#import "XMPPMessage+XEP_0071.h"
+#import "XMPPMessage+XEP_0085.h"
+#import "XMPPPresence+XEP_0152.h"
+#import "XMPPAutoPing.h"
+#import "XMPPPing.h"
