71b4bb857a7e978e33ab815ecb428592d1258d72
Indragie Karunaratne <i@indragie.com>
2013-01-08 21:13:41 -0700

    Deleted and moved a bunch of stuff

---
+++ XMPPFramework/XMPPFramework/XMPPFramework.h
@@ -0,0 +1,13 @@
+//
+//  XMPPFramework.h
+//  XMPPFramework
+//
+//  Created by Indragie Karunaratne on 2013-01-08.
+//  Copyright (c) 2013 nonatomic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMPPFramework : NSObject
+
+@end
---
+++ XMPPFramework/XMPPFramework/XMPPFramework.m
@@ -0,0 +1,13 @@
+//
+//  XMPPFramework.m
+//  XMPPFramework
+//
+//  Created by Indragie Karunaratne on 2013-01-08.
+//  Copyright (c) 2013 nonatomic. All rights reserved.
+//
+
+#import "XMPPFramework.h"
+
+@implementation XMPPFramework
+
+@end
