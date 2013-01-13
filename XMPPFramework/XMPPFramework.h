e8c6331068b581aa724cc07d6f55b31c821628e9
Indragie Karunaratne <i@indragie.com>
2013-01-13 15:40:33 -0700

    Use framework instead of static lib

--- XMPPFramework/XMPPFramework.h
+++ XMPPFramework/XMPPFramework.h
@@ -2,15 +2,17 @@
 //  XMPPFramework.h
 //  XMPPFramework
 //
-//  Created by Indragie Karunaratne on 2013-01-08.
+//  Created by Indragie Karunaratne on 2013-01-13.
 //  Copyright (c) 2013 nonatomic. All rights reserved.
 //
 
-#import "XMPP.h"
-#import "XMPPRoster.h"
-#import "XMPPRosterMemoryStorage.h"
-#import "XMPPvCardTempModule.h"
-#import "XMPPMessage+XEP_0071.h"
-#import "XMPPMessage+XEP_0085.h"
-#import "XMPPPresence+XEP_0153.h"
-#import "XMPPAutoPing.h"
+#import <Foundation/Foundation.h>
+#import <XMPPFramework/XMPP.h>
+#import <XMPPFramework/XMPPAutoPing.h>
+#import <XMPPFramework/XMPPPing.h>
+#import <XMPPFramework/XMPPPresence+XEP_0153.h>
+#import <XMPPFramework/XMPPMessage+XEP_0085.h>
+#import <XMPPFramework/XMPPMessage+XEP_0071.h>
+#import <XMPPFramework/XMPPvCardTempModule.h>
+#import <XMPPFramework/XMPPRoster.h>
+#import <XMPPFramework/XMPPRosterMemoryStorage.h>
